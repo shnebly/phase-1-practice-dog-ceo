f10635906ecf9ccb664133bd6c9f8a8149217719
Shelby Morris <shelbynicole.morris@gmail.com>
2023-03-11 14:26:03 -0700

    completed tasts, challenge 4 is messy, unsure of simplification

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ function getDogImages(){
     fetch(imgUrl)
         .then(resp => resp.json())
         .then(data => {
-            console.log(data.message);
+            // console.log(data.message);
             const dogImageDiv = document.getElementById('dog-image-container');
             data.message.forEach(url => {
                 const img = document.createElement('img');
@@ -17,12 +17,63 @@ function getDogImages(){
         })    
 }
 getDogImages();
+let breedKeys = [];
+let ulBreeds = document.getElementById('dog-breeds')
 
+//Challege 2 fetch breeds and add them to the DOM
+function getDogBreeds(){
+    const breedUrl = "https://dog.ceo/api/breeds/list/all";
+    fetch(breedUrl)
+        .then(resp => resp.json())
+        .then(data =>{
+            // console.log(data.message) //this returns an oject of breeds...
+            const breedObj = data.message
+            breedKeys = Object.keys(breedObj);
+            // console.log(breedKeys) //breedKeys is now an array of 98 breeds
+            breedKeys.map(breed => {
+                const liBreeds = document.createElement('li')
+                liBreeds.innerText = breed;
+                ulBreeds.append(liBreeds)
+            })
+                
+        })
+}
+getDogBreeds();
 
+//Challenge 3 when user clicks on an li it changes color
+document.addEventListener('click', e => {
+    if (e.target.matches('li')){
+        e.target.style.color = 'blue'
+    }
+})
 
+//Challenge 4 allow user to filter
+function filterBreed(letter){
+    ulBreeds.innerHTML = ''
+    letter.map(breed => {
+        const li = document.createElement('li')
+        li.innerText = breed;
+        ulBreeds.append(li)
+    })
+}
 
+const dropDown = document.getElementById("breed-dropdown")
+dropDown.addEventListener('change', e=> {
+    if(e.target.value === 'a'){
+        const A = breedKeys.filter(breed => breed[0] === 'a')
+        filterBreed(A)
+    } else if (e.target.value === 'b'){
+        const B = breedKeys.filter(breed => breed[0] === 'b')
+        filterBreed(B)
+    } else if (e.target.value === 'c'){
+        const C = breedKeys.filter(breed => breed[0] === 'c')
+        filterBreed(C)
+    } else if (e.target.value === 'd'){
+        const D = breedKeys.filter(breed => breed[0] === 'd')
+        filterBreed(D)
+    }
 
-
+})
 
 
 
