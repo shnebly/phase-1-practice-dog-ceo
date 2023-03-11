0ed63d78e960e42eee995aacd9c9c019745d06a6
Shelby Morris <shelbynicole.morris@gmail.com>
2023-03-11 13:11:34 -0700

    fetch URL and attach images to DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 console.log('%c HI', 'color: firebrick')
+window.addEventListener('DOMContentLoaded', (e) =>{
+
+//Challenge 1 fetch images and add them to the DOM
+function getDogImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data.message);
+            const dogImageDiv = document.getElementById('dog-image-container');
+            data.message.forEach(url => {
+                const img = document.createElement('img');
+                img.src = url;
+                dogImageDiv.append(img)
+            })
+        })    
+}
+getDogImages();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log('DOM Loaded')
+})
